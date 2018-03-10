ca76aa30445416c05277376a8c96f568fca79e66
MrMessy77 <313691730@qq.com>
2018-03-10 09:55:39 +0800

    整理

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ const message = {
     iotId: 'aaaaa',
     attribute: 'bbbbb',
     payload: {
-        value: 'test'
+        value: Math.random().toString(36)
     }
 };
 
@@ -49,105 +49,105 @@ describe('server', function() {
     });
 });
 
-// describe('update', function() {
-//     it('update(app -> service)', function(done) {
-//         service1.on('$update',(payload) => {
-//             if (payload.tar == service1.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
-//                 done();
-//             }
-//             else {
-//                 done(new Error('update failed'));
-//             }
-//         });
-//
-//         app1.update({
-//             channel: '$iot',
-//             params: {iotId: message.iotId, attribute: message.attribute},
-//             payload: message.payload,
-//             options: {}
-//         });
-//     });
-// });
-//
-// describe('notify', function() {
-//     it('notify(service -> one app)', function(done) {
-//         app1.on('$notify',(payload) => {
-//             if (payload.tar == service1.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
-//                 done();
-//             }
-//             else {
-//                 done(new Error('notify failed'));
-//             }
-//         });
-//
-//         service1.notify({
-//             tar: app1.appToken,
-//             src: app1.appToken,
-//             channel: '$iot',
-//             params: {iotId: message.iotId, attribute: message.attribute},
-//             payload: message.payload,
-//             options: {}
-//         });
-//     });
-//     it('notify(service -> more app)', function(done) {
-//         service1.on('$update',(payload) => {
-//             service1.notify({
-//                 tar: payload.tar,
-//                 src: service1.appToken,
-//                 channel: '$iot',
-//                 params: {iotId: payload.iotId, attribute: payload.attribute},
-//                 payload: payload.payload,
-//                 options: {}
-//             });
-//         });
-//
-//         app1.update({
-//             channel: '$iot',
-//             params: {iotId: message.iotId, attribute: message.attribute},
-//             payload: message.payload,
-//             options: {}
-//         });
-//         app2.update({
-//             channel: '$iot',
-//             params: {iotId: message.iotId, attribute: message.attribute},
-//             payload: message.payload,
-//             options: {}
-//         });
-//         app3.update({
-//             channel: '$iot',
-//             params: {iotId: message.iotId, attribute: message.attribute},
-//             payload: message.payload,
-//             options: {}
-//         });
-//
-//         app1.on('$notify',(payload) => {
-//             if (payload.tar == app1.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
-//                 console.log('notify app1 success');
-//             }
-//             else {
-//                 console.log('notify app1 failed');
-//             }
-//         });
-//         app2.on('$notify',(payload) => {
-//             if (payload.tar == app2.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
-//                 console.log('notify app2 success');
-//             }
-//             else {
-//                 console.log('notify app2 failed');
-//             }
-//         });
-//         app3.on('$notify',(payload) => {
-//             if (payload.tar == app3.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
-//                 console.log('notify app3 success');
-//             }
-//             else {
-//                 console.log('notify app3 failed');
-//             }
-//         });
-//
-//         done();
-//     });
-// });
+describe('update', function() {
+    it('update(app -> service)', function(done) {
+        service1.on('$update',(payload) => {
+            if (payload.tar == service1.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
+                done();
+            }
+            else {
+                done(new Error('update failed'));
+            }
+        });
+
+        app1.update({
+            channel: '$iot',
+            params: {iotId: message.iotId, attribute: message.attribute},
+            payload: message.payload,
+            options: {}
+        });
+    });
+});
+
+describe('notify', function() {
+    it('notify(service -> one app)', function(done) {
+        app1.on('$notify',(payload) => {
+            if (payload.tar == service1.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
+                done();
+            }
+            else {
+                done(new Error('notify failed'));
+            }
+        });
+
+        service1.notify({
+            tar: app1.appToken,
+            src: app1.appToken,
+            channel: '$iot',
+            params: {iotId: message.iotId, attribute: message.attribute},
+            payload: message.payload,
+            options: {}
+        });
+    });
+    it('notify(service -> more app)', function(done) {
+        service1.on('$update',(payload) => {
+            service1.notify({
+                tar: payload.tar,
+                src: service1.appToken,
+                channel: '$iot',
+                params: {iotId: payload.iotId, attribute: payload.attribute},
+                payload: payload.payload,
+                options: {}
+            });
+        });
+
+        app1.update({
+            channel: '$iot',
+            params: {iotId: message.iotId, attribute: message.attribute},
+            payload: message.payload,
+            options: {}
+        });
+        app2.update({
+            channel: '$iot',
+            params: {iotId: message.iotId, attribute: message.attribute},
+            payload: message.payload,
+            options: {}
+        });
+        app3.update({
+            channel: '$iot',
+            params: {iotId: message.iotId, attribute: message.attribute},
+            payload: message.payload,
+            options: {}
+        });
+
+        app1.on('$notify',(payload) => {
+            if (payload.tar == app1.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
+                console.log('notify app1 success');
+            }
+            else {
+                console.log('notify app1 failed');
+            }
+        });
+        app2.on('$notify',(payload) => {
+            if (payload.tar == app2.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
+                console.log('notify app2 success');
+            }
+            else {
+                console.log('notify app2 failed');
+            }
+        });
+        app3.on('$notify',(payload) => {
+            if (payload.tar == app3.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
+                console.log('notify app3 success');
+            }
+            else {
+                console.log('notify app3 failed');
+            }
+        });
+
+        done();
+    });
+});
 
 describe('req->rreq->rresp->resp', function() {
     it('req->rreq->rresp->resp', function(done) {
@@ -172,7 +172,7 @@ describe('req->rreq->rresp->resp', function() {
 
         app2.on('$rreq',(payload) => {
             app2.rresp({
-                src: payload.src,
+                tar: payload.tar,
                 channel: '$iot',
                 params: payload.params,
                 messageId: payload.messageId,
@@ -187,9 +187,9 @@ describe('req->rreq->rresp->resp', function() {
                 tar: app2.appToken,
                 channel: '$iot',
                 params: {iotId: message.iotId, attribute: message.attribute},
-                payload: {random: Math.random().toString(36)}
+                payload: message.payload
             }).then((payload) => {
-                if (payload.tar == app1.appToken && payload.params.iotId == message.iotId && payload.params.attribute == message.attribute) {
+                if (payload.payload.value == message.payload.value) {
                     // count += 1;
                     // if (count == 99) {
                         done();
