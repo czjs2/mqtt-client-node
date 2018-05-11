91f500c53989564500cbe1826ecf230db6c4e242
MrMessy77 <313691730@qq.com>
2018-05-11 14:03:48 +0800

    修改end方法

--- libs/baseNode.js
+++ libs/baseNode.js
@@ -119,10 +119,15 @@ class node extends EventEmitter {
      *
      * @param {function} cb eg.()=>{}
      */
-    end() {
+    end(timeout) {
         if (this.mqttClient) {
-            return new P((resolve) => {
+            return new P((resolve,reject) => {
+                let complete_handler = setTimeout(() => {
+                    reject('end timeout');
+                },timeout||2000);
+
                 this.mqttClient.end(true, () => {
+                    clearTimeout(complete_handler);
                     this.removeAllListeners();
                     resolve();
                 });
